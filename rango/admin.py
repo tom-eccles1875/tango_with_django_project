76ac278ae65ed16c830b1c2031f33e2840cf1f50
Tom Eccles <2660405e@student.gla.ac.uk>
2023-02-05 19:51:21 +0000

    sixth commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 
 
@@ -10,5 +11,6 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 # Update the registration to include this customised interface
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
 
 admin.site.register(Page, PageAdmin)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'rango:login'
 
 # Application definition
 
@@ -105,6 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
+
 
 
 # Internationalization
